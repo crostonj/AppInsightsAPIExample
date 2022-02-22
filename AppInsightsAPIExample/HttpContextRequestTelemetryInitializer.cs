d624982753b466029c847431b14ab85d9ac0a1aa
Jeff Croston <jcroston@3cloudsolutons.com>
2022-02-22 16:42:46 -0700

    Add project files.

---
+++ AppInsightsAPIExample/HttpContextRequestTelemetryInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace AppInsightsAPIExample
+{
+    public class HttpContextRequestTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public HttpContextRequestTelemetryInitializer(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor =
+                httpContextAccessor ??
+                throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public void Initialize(Microsoft.ApplicationInsights.Channel.ITelemetry telemetry)
+        {
+            var requestTelemetry = telemetry as RequestTelemetry;
+            if (requestTelemetry == null) return;
+
+            var claims = this.httpContextAccessor.HttpContext.User.Claims;
+            Claim oidClaim = claims.FirstOrDefault(claim => claim.Type == "oid");
+            requestTelemetry.Properties.Add("UserOid", oidClaim?.Value);
+        }
+    }
+}
---
+++ AppInsightsAPIExample/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppInsightsAPIExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddApplicationInsightsTelemetry(Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+            services.AddSingleton<ITelemetryInitializer, MyTelemetryInitializer>();
+            services.AddSingleton<ITelemetryInitializer, HttpContextRequestTelemetryInitializer>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
